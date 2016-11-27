19e6804a6149ab28ae53880f0c9cf31c9b7e9522
Colin Beeby <colinbeeby.leeds@gmail.com>
2016-11-27 15:58:59 +0000

    Added artist selection list

--- BBPlayer/BBPlayer.py
+++ BBPlayer/BBPlayer.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
 '''
 Created on 20 Nov 2016
 
 @author: colin
 '''
-import time
+from os import listdir
+from os.path import isdir, isfile, join
 
 from Tkinter import *
 
@@ -15,17 +17,19 @@ class GUIInterfaceController(object):
     Class to provide functions to interface to the MplayerController
     '''
     
+    MUSIC_PATH = '/home/colin/Music/'
+    
     def __init__(self):
         '''
         Constructor
-        '''
+        '''   
         self.mplayer = MplayerController(self.finishedPlayingTrack)
         self.playlist = PlaylistController()
     
         self.root = Tk()
         self.root.title('BBPlayer')
         #self._fullScreen()
-        self.root.geometry("500x500")
+        self.root.geometry("500x500") #take this out for production
         #self._hideTitleBar() PUT THIS BACK IN FOR PRODUCTION
         
         #Label(text='Hello World!!').pack()
@@ -36,7 +40,11 @@ class GUIInterfaceController(object):
         Button(self.root, text='Quit', command=self.quit).pack()
         self.trackDetails = Entry(self.root, width=100)
         self.trackDetails.pack()
-        #self.root.protocol("WM_DELETE_WINDOW", self.quit)
+        self.artistListBox = Listbox(self.root, selectmode=SINGLE)
+        self.artistListBox.pack()
+        self.artistList = []
+        self._getArtists()
+        
         self.playListLength = self.playlist.addDirectoryToPlaylist('/home/colin/Music/Inkubus Sukkubus/Supernature')
         if self.playListLength > 0:
             self.nextTrack = self.playlist.getNextTrack()
@@ -52,9 +60,19 @@ class GUIInterfaceController(object):
     def _hideTitleBar(self):
         self.root.overrideredirect(True)
         
+    def _getArtists(self):
+        if isdir(self.MUSIC_PATH):
+            #dirsInDir = []
+            for artistDir in listdir(self.MUSIC_PATH):
+                if isdir(join(self.MUSIC_PATH, artistDir)):
+                    #dirsInDir.appand(join(self.MUSIC_PATH, albumDir))
+                    self.artistListBox.insert(END, artistDir)
+                    self.artistList.append(join(self.MUSIC_PATH, artistDir))
+        #return dirsInDir
+                
+        
     def finishedPlayingTrack(self):
         self._playNextTrack()
-        pass
     
     def play(self):
         if not self.nextTrack == None:
